9ca96cf7529173cb145a33e5907c4f2b584a2be2
jacob l <jacobinmt47@outlook.com>
2021-07-25 12:29:10 -0600

    updateing test cases in main

--- main.cpp
+++ main.cpp
@@ -16,11 +16,12 @@ void printtree(Node *n){
     }
 }
 int main(int argc,char** argv){
-   /* Node *root = insertnb(NULL, 20);
+    Node *root = insertnb(NULL, 20);
     root = insertnb(root, 10);
     root = insertnb(root, 30);
+    root = insertnb(root,5);
     printtree(root);
-    */
+
     // try a straight tree 10-20-30
     Node *rt2 = insertnb(NULL,10);
     rt2 = insertnb(rt2,20);
@@ -28,5 +29,6 @@ int main(int argc,char** argv){
     rt2 = insertnb(rt2,40);
     cout<<"-- next tree --"<<endl;
     printtree(rt2);
+    
     return 0;
 }
